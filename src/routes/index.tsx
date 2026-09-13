c1e003f36462e90d449db4e057843d9d5f8a4a58
Shravani Pulate <shravanipulate30@gmail.com>
2026-09-14 02:52:11 +0530

    Load fixed v15 experience recorder

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -90,10 +90,10 @@ function Index() {
                           visits.src = "/experience-upgrade-v14.js";
                           visits.async = false;
                           visits.onload = () => {
-                            if (doc.getElementById("birthday-upgrade-v16-script")) return;
+                            if (doc.getElementById("birthday-upgrade-v15-script")) return;
                             const recorder = doc.createElement("script");
-                            recorder.id = "birthday-upgrade-v16-script";
-                            recorder.src = "/experience-upgrade-v16.js";
+                            recorder.id = "birthday-upgrade-v15-script";
+                            recorder.src = "/experience-upgrade-v15.js";
                             recorder.async = false;
                             doc.body.appendChild(recorder);
                           };

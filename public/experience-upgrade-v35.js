eed637277c96383fe11ac847bbb998327a27d7cc
Shravani Pulate <shravanipulate30@gmail.com>
2026-09-15 14:23:35 +0530

    Add cache-busted YouTube UI loader

---
+++ public/experience-upgrade-v35.js
@@ -0,0 +1,9 @@
+(() => {
+  'use strict';
+  if (window.__birthdayYouTubeStylishV35) return;
+  window.__birthdayYouTubeStylishV35 = true;
+  const s = document.createElement('script');
+  s.src = '/experience-upgrade-v34.js?v=20260915-wow1';
+  s.async = false;
+  document.body.appendChild(s);
+})();

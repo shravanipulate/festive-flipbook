413ea3ee1dc188b904bd09ccd3411a95f60e01b1
Shravani Pulate <shravanipulate30@gmail.com>
2026-09-15 14:24:06 +0530

    Temporary loader for stylish YouTube search

---
+++ public/experience-upgrade-v38.js
@@ -0,0 +1,7 @@
+(() => {
+'use strict';
+if (document.getElementById('birthday-youtube-stylish-v34-box')) return;
+const s=document.createElement('script');
+s.src='/experience-upgrade-v34.js?v=20260915-final2';
+document.body.appendChild(s);
+})();

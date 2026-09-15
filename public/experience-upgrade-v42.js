0ad0f376221aa587abddcf0c7a3924549f598855
Shravani Pulate <shravanipulate30@gmail.com>
2026-09-15 14:24:31 +0530

    Wire stylish YouTube search helper

---
+++ public/experience-upgrade-v42.js
@@ -0,0 +1,8 @@
+(() => {
+'use strict';
+const load=()=>{
+ const p=document.getElementById('bgmPanel');
+ if(!p||document.getElementById('birthday-youtube-stylish-v34-box')) return;
+ const s=document.createElement('script');s.src='/experience-upgrade-v34.js?v=20260915-wow-final3';s.async=false;document.body.appendChild(s);
+};load();new MutationObserver(load).observe(document.documentElement,{childList:true,subtree:true});
+})();

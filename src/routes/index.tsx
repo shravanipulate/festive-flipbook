2bfa503f79c3099aca060df7d45275fd65d8cd72
Shravani Pulate <shravanipulate30@gmail.com>
2026-09-15 22:09:57 +0530

    Add Controls dropdown to recorder UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -26,6 +26,7 @@ const UPGRADE_SCRIPTS = [
   "experience-upgrade-v16.js",
   "experience-upgrade-v17.js",
   "experience-upgrade-v22.js",
+  "experience-recorder-controls-v20.js",
 ] as const;
 
 export const Route = createFileRoute("/")({
